dde465767d1409586504116f5a91d219211d9b0c
sheyisax <olusheyax@gmail.com>
2023-07-19 21:31:32 +0100

    new com

--- controllers/registration.js
+++ controllers/registration.js
@@ -7,112 +7,131 @@ const User = require("../models/users");
 const Hackathon = require("../models/hackusers");
 
 // mail sending controller
-const mailSender = (usermail, firstname) => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL,
-      pass: process.env.MAILPASS, // naturally, replace both with your real credentials or an application-specific password
-    },
-  });
+const mailSender = async(usermail, firstname) => {
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.MAILPASS, // naturally, replace both with your real credentials or an application-specific password
+        },
+    });
 
-  var mailGenerator = new Mailgen({
-    theme: "default",
-    product: {
-      // Appears in header & footer of e-mails
-      name: "SED2023 Team",
-      link: "https://mailgen.js/",
-      // Optional product logo
-      // logo: 'https://mailgen.js/img/logo.png'
-    },
-  });
+    await new Promise((resolve, reject) => {
+        // verify connection configuration
+        transporter.verify(function(error, success) {
+            if (error) {
+                console.log(error);
+                reject(error);
+            } else {
+                console.log("Server is ready to take our messages");
+                resolve(success);
+            }
+        });
+    });
 
-  // Mail body
-  var email = {
-    body: {
-      name: firstname,
-      intro: "Welcome to SED2023 we are excited to have you join us ",
-      // action: {
-      //     instructions: 'To get started with Mailgen, please click here:',
-      //     // button: {
-      //     //     color: '#22BC66', // Optional action button color
-      //     //     text: 'Confirm your account',
-      //     //     link: 'https://mailgen.js/confirm?s=d9729feb74992cc3482b350163a1a010'
-      //     // }
-      // },
-      outro:
-        "Need help, or have questions? Just reply to this email, we'd love to help.",
-    },
-  };
+    var mailGenerator = new Mailgen({
+        theme: "default",
+        product: {
+            // Appears in header & footer of e-mails
+            name: "SED2023 Team",
+            link: "https://mailgen.js/",
+            // Optional product logo
+            // logo: 'https://mailgen.js/img/logo.png'
+        },
+    });
 
-  // Generate an HTML email with the provided contents
-  var emailBody = mailGenerator.generate(email);
+    // Mail body
+    var email = {
+        body: {
+            name: firstname,
+            intro: "Welcome to SED2023 we are excited to have you join us ",
+            // action: {
+            //     instructions: 'To get started with Mailgen, please click here:',
+            //     // button: {
+            //     //     color: '#22BC66', // Optional action button color
+            //     //     text: 'Confirm your account',
+            //     //     link: 'https://mailgen.js/confirm?s=d9729feb74992cc3482b350163a1a010'
+            //     // }
+            // },
+            outro: "Need help, or have questions? Just reply to this email, we'd love to help.",
+        },
+    };
 
-  // Generate the plaintext version of the e-mail (for clients that do not support HTML)
-  var emailText = mailGenerator.generatePlaintext(email);
+    // Generate an HTML email with the provided contents
+    var emailBody = mailGenerator.generate(email);
 
-  const mailOptions = {
-    from: "saxdamee@gmail.com",
-    to: usermail,
-    subject: "Sustainable Energy Days 2023",
-    html: emailBody,
-  };
+    // Generate the plaintext version of the e-mail (for clients that do not support HTML)
+    var emailText = mailGenerator.generatePlaintext(email);
 
-  transporter.sendMail(mailOptions, function (error, info) {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log("Email sent: " + info.response);
-    }
-  });
+    const mailOptions = {
+        from: "saxdamee@gmail.com",
+        to: usermail,
+        subject: "Sustainable Energy Days 2023",
+        html: emailBody,
+    };
+
+
+    await new Promise((resolve, reject) => {
+        // send mail
+        transporter.sendMail(mailOptions, function(error, info) {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log("Email sent: " + info.response);
+            }
+        });
+
+    });
+
+
 };
 
 // user registration
-router.post("/user/register", async (req, res) => {
-  const user = new User({
-    firstname: req.body.firstname,
-    lastname: req.body.lastname,
-    email: req.body.email,
-    phone: req.body.phone,
-    city: req.body.city,
-    country: req.body.country,
-  });
+router.post("/user/register", async(req, res) => {
+    const user = new User({
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        email: req.body.email,
+        phone: req.body.phone,
+        city: req.body.city,
+        country: req.body.country,
+    });
 
-  //Check if email or phone number already exists
+    //Check if email or phone number already exists
 
-  // save user
-  result = await user.save();
-  const { ...data } = await result.toJSON();
+    // save user
+    result = await user.save();
+    const {...data } = await result.toJSON();
 
-  //Send Email
-  mailSender(req.body.email, req.body.firstname);
-  res.status(201).json(data);
+    //Send Email
+    mailSender(req.body.email, req.body.firstname);
+    res.status(201).json(data);
 });
 
-router.get("/users", async (req, res) => {
-  try {
-    const users = await User.find();
-    res.json(users);
-  } catch (err) {
-    console.error(error);
-    res.status(500).json({ message: " Server Error" });
-  }
+router.get("/users", async(req, res) => {
+    try {
+        const users = await User.find();
+        res.json(users);
+    } catch (err) {
+        console.error(error);
+        res.status(500).json({ message: " Server Error" });
+    }
 });
 
-router.post("/hack/register", async (req, res) => {
-  const user = new Hackathon({
-    firstname: req.body.firstname,
-    lastname: req.body.lastname,
-    email: req.body.email,
-    phone: req.body.phone,
-    city: req.body.city,
-    country: req.body.country,
-  });
+router.post("/hack/register", async(req, res) => {
+    const user = new Hackathon({
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        email: req.body.email,
+        phone: req.body.phone,
+        city: req.body.city,
+        country: req.body.country,
+    });
 
-  // save user
-  result = await user.save();
-  const { data } = await result.toJSON();
-  res.send(data);
+    // save user
+    result = await user.save();
+    const { data } = await result.toJSON();
+    res.send(data);
 });
 
 module.exports = router;
